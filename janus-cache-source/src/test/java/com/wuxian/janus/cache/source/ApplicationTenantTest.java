93bbce8da3e96f37efd5ba5497f93955ae4000d0
jyl550 <jyl550@163.com>
2020-05-31 22:34:17 +0800

    根据role和permissionTemplate的multiple构造方式同步修改逻辑

--- janus-cache-source/src/test/java/com/wuxian/janus/cache/source/ApplicationTenantTest.java
+++ janus-cache-source/src/test/java/com/wuxian/janus/cache/source/ApplicationTenantTest.java
@@ -53,22 +53,22 @@ public class ApplicationTenantTest {
                                 .setNative(NativePermissionTemplateEnum.CREATE_INNER_GROUP, "205")
                                 .setNative(NativePermissionTemplateEnum.CREATE_ROLE, "206")
                                 .addItem(
-                                        UserGroup.byId("6", "stdUG", "AAAOutGroup", "ref100").addItem(Role.byId("5", "xxxRole")),
+                                        UserGroup.byId("6", "stdUG", "AAAOutGroup", "ref100").addItem(Role.byId("5", "xxxRole", false)),
                                         UserGroup.byId("7", "bbbUG").addItem(
-                                                Role.byId("6", "yyyRole"), Role.byId("7", "zzzRole"), User.byId("1")))
+                                                Role.byId("6", "yyyRole", false), Role.byId("7", "zzzRole", true), User.byId("1")))
                                 .addItem(
                                         new Role(NativeRoleEnum.ALL_PERMISSION).addItem(User.byId("1")),
                                         new Role(NativeRoleEnum.TENANT_MAINTAINER).addItem(
                                                 Permission.byId("715", "p-15-del"), Permission.byId("205", NativePermissionTemplateEnum.CREATE_INNER_GROUP),
                                                 Permission.byId("206", NativePermissionTemplateEnum.CREATE_ROLE), User.byId("1")
                                         ),
-                                        Role.byId("5", "xxxRole").addItem(
+                                        Role.byId("5", "xxxRole", false).addItem(
                                                 Permission.byId("715", "p-15-del"), Permission.byId("716", "p-16-add")
                                         ),
-                                        Role.byId("6", "yyyRole").addItem(
+                                        Role.byId("6", "yyyRole", false).addItem(
                                                 Permission.byId("716", "p-16-add"), Permission.byId("717", "p-17-edit")
                                         ),
-                                        Role.byId("7", "zzzRole", "MyOrganization", "ref8002", null).addItem(
+                                        Role.byId("7", "zzzRole", true, "MyOrganization", "ref8002", null).addItem(
                                                 Permission.byId("719", "p-19-update-org", "MyOrganization", "ref8002")))
                                 .addItem(
                                         Permission.byId("714", "p-14-synchronism"),
@@ -90,13 +90,13 @@ public class ApplicationTenantTest {
         ApplicationGroup applicationGroup = new ApplicationGroup().addItem(
                 Application.byId("1").addItem(
                         Tenant.byId("10").addItem(
-                                new Role("deptMgr").addItem(
+                                new Role("deptMgr", false).addItem(
                                         User.byId("100"),
                                         new Permission("abc"),
                                         new Permission("def"),
                                         new Permission("xyz")
                                 ),
-                                new Role("deptEmployee").addItem(
+                                new Role("deptEmployee", false).addItem(
                                         User.byId("101"),
                                         User.byId("102"),
                                         new Permission("abc"),
@@ -119,13 +119,13 @@ public class ApplicationTenantTest {
         ApplicationGroup applicationGroup = new ApplicationGroup().addItem(
                 Application.byId("1").addItem(
                         Tenant.byId("10").addItem(
-                                new Role("deptMgr").addItem(
+                                new Role("deptMgr", false).addItem(
                                         User.byId("100"),
                                         new Permission("abc"),
                                         new Permission("def"),
                                         new Permission("xyz")
                                 ),
-                                new Role("deptEmployee").addItem(
+                                new Role("deptEmployee", false).addItem(
                                         User.byId("101"),
                                         User.byId("102"),
                                         new Permission("abc"),
