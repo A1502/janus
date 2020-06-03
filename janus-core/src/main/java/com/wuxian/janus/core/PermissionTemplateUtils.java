413d96f94fc85f7fcb0e7e9a6a6e05214f2ac58d
A1502 <jyl550@qq.com>
2020-06-03 20:57:51 +0800

    增加relationAllowed方法

--- janus-core/src/main/java/com/wuxian/janus/core/PermissionTemplateUtils.java
+++ janus-core/src/main/java/com/wuxian/janus/core/PermissionTemplateUtils.java
@@ -1,10 +1,10 @@
 package com.wuxian.janus.core;
 
-import com.wuxian.janus.entity.PermissionTemplateEntity;
 import com.wuxian.janus.core.critical.NativePermissionTemplateEnum;
+import com.wuxian.janus.entity.PermissionTemplateEntity;
 
 public class PermissionTemplateUtils {
-    
+
     public static PermissionTemplateEntity getNativePermissionTemplate(NativePermissionTemplateEnum permissionTemplate) {
         PermissionTemplateEntity entity = new PermissionTemplateEntity();
 
@@ -12,5 +12,21 @@ public class PermissionTemplateUtils {
         entity.setPermissionLevel(permissionTemplate.getLevel().getCode());
         entity.setPermissionType(permissionTemplate.getPermissionType());
         return entity;
+    }
+
+
+    public static boolean relationAllowed(String outerObjectTypeCodeOfPermissionTemplate, String outerObjectTypeCodeOfPermission) {
+        return relationAllowedInner(outerObjectTypeCodeOfPermissionTemplate, outerObjectTypeCodeOfPermission);
+    }
+
+    /**
+     * 根据outerObjectType判定Permission和PermissionTemplate可否关联
+     *
+     * @param outerObjectTypeOfPermissionTemplate PermissionTemplate的outerObjectType
+     * @param outerObjectTypeOfPermission         Permission的outerObjectType
+     * @return 是否允许
+     */
+    private static <T> boolean relationAllowedInner(T outerObjectTypeOfPermissionTemplate, T outerObjectTypeOfPermission) {
+        return outerObjectTypeOfPermissionTemplate == outerObjectTypeOfPermission;
     }
 }
