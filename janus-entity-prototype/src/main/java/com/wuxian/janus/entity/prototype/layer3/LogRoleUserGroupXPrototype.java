3276b0f6651460db518c4d57a09e7effd2e69c35
A1502 <jyl550@qq.com>
2020-06-05 09:18:20 +0800

    分层更名

---
+++ janus-entity-prototype/src/main/java/com/wuxian/janus/entity/prototype/layer3/LogRoleUserGroupXPrototype.java
@@ -0,0 +1,36 @@
+package com.wuxian.janus.entity.prototype.layer3;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.wuxian.janus.entity.prototype.ControlPrototype;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+/**
+ * Prototype类
+ *
+ * @author wuxian
+ * @email
+ * @date 2019/07/09
+ */
+@Accessors(chain = true)
+@Setter
+@Getter
+@NoArgsConstructor
+@TableName("log_role_user_group_x")
+public class LogRoleUserGroupXPrototype<ID, UID> extends ControlPrototype<ID, UID> {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableField("role_id")
+    @ApiModelProperty(value = "角色id", example = "1")
+    private ID roleId;
+
+    @TableField("user_group_id")
+    @ApiModelProperty(value = "用户组id", example = "1")
+    private ID userGroupId;
+
+}
