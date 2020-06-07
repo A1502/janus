32643c08d88518cbe58a2f37c4402888e954b504
jyl550 <jyl550@163.com>
2020-06-07 13:32:04 +0800

    增加hashCode,equals重写

--- janus-struct-classic/src/main/java/com/wuxian/janus/struct/primary/IdType.java
+++ janus-struct-classic/src/main/java/com/wuxian/janus/struct/primary/IdType.java
@@ -12,4 +12,14 @@ public class IdType extends BaseIdType<Long> {
     public void setStringValue(String format) {
         setValue(Long.parseLong(format));
     }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
 }
