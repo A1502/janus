f56bcac9ea221760e239cdb3116a6cbdefdb5347
jyl550 <jyl550@163.com>
2020-06-08 02:31:22 +0800

    清点好22个部分

--- janus-cache-model/src/main/java/com/wuxian/janus/cache/model/extract/SourceExtractor.java
+++ janus-cache-model/src/main/java/com/wuxian/janus/cache/model/extract/SourceExtractor.java
@@ -2,7 +2,10 @@ package com.wuxian.janus.cache.model.extract;
 
 import com.wuxian.janus.cache.model.source.ApplicationGroup;
 import com.wuxian.janus.cache.model.extract.id.IdGeneratorFactory;
+import com.wuxian.janus.cache.model.source.Role;
 import com.wuxian.janus.core.cache.provider.DirectAccessControlSource;
+import com.wuxian.janus.core.cache.provider.TenantMap;
+import com.wuxian.janus.struct.primary.IdType;
 
 public class SourceExtractor {
 
@@ -15,30 +18,38 @@ public class SourceExtractor {
     public DirectAccessControlSource extract(ApplicationGroup applicationGroup) {
         DirectAccessControlSource result = new DirectAccessControlSource();
 
-        //STEP1:提取(19)outerObjectType DONE
+        //STEP1:提取(20)outerObjectType DONE
         OuterObjectTypeExtractor.extract(applicationGroup, this.idGeneratorFactory, result);
 
-        //STEP2-1:提取(20)outerObject DONE
-        //STEP2-2:提取(21)userOuterObjectX DONE
+        //STEP2-1:提取(21)outerObject DONE
+        //STEP2-2:提取(22)userOuterObjectX DONE
         UserAndOuterObjectExtractor.extract(applicationGroup, this.idGeneratorFactory, result);
 
-        //STEP3:提取(18)permissionTemplate DONE
+        //STEP3:提取(19)permissionTemplate DONE
         PermissionTemplateExtractor.extract(applicationGroup, this.idGeneratorFactory, result);
 
-        //STEP4:提取(6)singlePermission,(12)multiplePermission DONE
+        //STEP4:提取(7)singlePermission,(13)multiplePermission DONE
         PermissionExtractor.extract(applicationGroup, this.idGeneratorFactory, result);
 
-        //STEP5:提取(8)singleRole,(14)multipleRole DONE
-        //(7)singleRolePermission,(13)multipleRolePermission DONE
-        RoleExtractor.extract(applicationGroup, this.idGeneratorFactory, result);
+        //STEP5:提取(9)singleRole,(15)multipleRole DONE
+        TenantMap<IdType, Role> roleTenantMap
+                = RoleExtractor.extract(applicationGroup, this.idGeneratorFactory, result);
 
-        //STEP6:提取(4)userGroup DONE
+        //(8)singleRolePermissionX,(14)multipleRolePermissionX DONE
+        RolePermissionExtractor.extract(roleTenantMap, idGeneratorFactory, result);
+
+        //TODO
+        //STEP6:提取(1)ScopeSingleRoleUserX,(2)ScopeMultipleRoleUserX
+        //(10)SingleRoleOtherX,(12)SingleRoleUserX
+        //(16)MultipleRoleOtherX,(18)MultipleRoleUserX
+        RoleRelationAndScopeExtractor.extract(roleTenantMap, this.idGeneratorFactory, result);
+
+        //STEP7:提取(4)userGroup DONE TODO 改为返回值模式
         UserGroupExtractor.extract(applicationGroup, this.idGeneratorFactory, result);
 
-        //STEP7:提取roleRelation TODO
-        RoleRelationExtractor.extract(applicationGroup, this.idGeneratorFactory, result);
-
-        //STEP8:提取userGroupUser TODO
+        //STEP8:提取(3)ScopeUserGroupUserX
+        //(5)UserGroupUserX,(6)UserGroupOtherX
+        //(11)SingleRoleUserGroupX,(17)MultipleRoleUserGroupX
         UserGroupUserExtractor.extract(applicationGroup, this.idGeneratorFactory, result);
 
         return result;
