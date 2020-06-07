166a7a790c788a4f796a96827cd868043fefd093
jyl550 <jyl550@163.com>
2020-06-07 20:59:08 +0800

    增加测试用例

--- janus-cache-model/src/test/java/com/wuxian/janus/cache/model/UserGroupTest.java
+++ janus-cache-model/src/test/java/com/wuxian/janus/cache/model/UserGroupTest.java
@@ -2,16 +2,20 @@ package com.wuxian.janus.cache.model;
 
 import com.wuxian.janus.cache.model.source.User;
 import com.wuxian.janus.cache.model.source.UserGroup;
+import com.wuxian.janus.core.basis.ErrorCodeException;
+import com.wuxian.janus.core.critical.Access;
 import com.wuxian.janus.core.critical.LevelEnum;
 import org.junit.Assert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class UserGroupTest {
 
 
     @Test
-    @DisplayName("测试合并")
+    @DisplayName("测试正常合并")
     void testMerge() {
 
         UserGroup one = UserGroup.byId("0");
@@ -26,6 +30,22 @@ public class UserGroupTest {
 
         //users也以Map方式合并了
         Assert.assertEquals(one.getUsers().size(), 3);
+    }
+
+    @Test
+    @DisplayName("测试Access重复的合并,应异常")
+    void testMergeAccessConflict() {
+
+        UserGroup one = UserGroup.byId("0");
+        one.setAccess(new Access(true, false, false, false, false));
+        //id相同才可合并，否则报错
+        UserGroup another = UserGroup.byId("0");
+        another.setAccess(new Access(true, true, false, false, false));
+
+        //Access在one和another中被设置了两次应该会冲突，无法合并的
+        assertThrows(ErrorCodeException.class
+                , () -> one.mergeFrom(another)
+                , "未抛出ErrorCodeException异常");
     }
 }
 
