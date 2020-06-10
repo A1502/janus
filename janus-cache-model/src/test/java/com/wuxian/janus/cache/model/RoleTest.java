f852e3b67a04d7c9b4697a01113c0dcde1796610
A1502 <jyl550@qq.com>
2020-06-10 16:14:52 +0800

    testMergeRole用例补完整

--- janus-cache-model/src/test/java/com/wuxian/janus/cache/model/RoleTest.java
+++ janus-cache-model/src/test/java/com/wuxian/janus/cache/model/RoleTest.java
@@ -7,16 +7,20 @@ import com.wuxian.janus.core.critical.Access;
 import com.wuxian.janus.core.critical.AccessControl;
 import com.wuxian.janus.core.critical.LevelEnum;
 import com.wuxian.janus.core.critical.NativeRoleEnum;
+import com.wuxian.janus.struct.layer1.RoleOtherXStruct;
 import com.wuxian.janus.struct.layer1.RoleUserXStruct;
+import com.wuxian.janus.struct.layer1.ScopeRoleUserXStruct;
 import com.wuxian.janus.struct.primary.ApplicationIdType;
 import com.wuxian.janus.struct.primary.IdType;
 import com.wuxian.janus.struct.primary.TenantIdType;
+import com.wuxian.janus.struct.prototype.layer1.ScopeRoleUserXPrototype;
 import org.junit.Assert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class RoleTest {
@@ -35,6 +39,12 @@ public class RoleTest {
         String roleBId = "81";
         String roleAllPermissionId = "88";
 
+        String scope_a = "scope_a";
+        String scope_b = "scope_b";
+        String scope_c = "scope_c";
+        String scope_d = "scope_d";
+        String scope_null = null;
+
         ApplicationGroup applicationGroup = new ApplicationGroup().addItem(
                 Application.byId(appId).addItem(
                         Tenant.byId(tId).addItem(
@@ -44,7 +54,7 @@ public class RoleTest {
                                         .addItem(
                                                 //role关联user默认是THREE
                                                 //加入了a,b两个scope
-                                                User.byId(user100, "a_scope", "b_scope"),
+                                                User.byId(user100, scope_a, scope_b),
 
                                                 //new Permission只是用来做陪衬，提高仿真度
                                                 new Permission("abc"),
@@ -54,7 +64,7 @@ public class RoleTest {
                                         .addItem(
                                                 //再次指定role关联user，且指定了AccessControl细节
                                                 //加入了a,c两个scope
-                                                User.byId(user100, "a_scope", "c_scope")
+                                                User.byId(user100, scope_a, scope_c)
                                                 , new AccessControl(new boolean[]{false, false, false, false, false
                                                         , false, false, false, false, true})),
 
@@ -70,7 +80,7 @@ public class RoleTest {
                                         //经过考虑觉得超过ag:root级别的能力也是可以的,没什么不妥
                                         //由于完全控制，可能会执行删除ALL_PERMISSION,
                                         //删除后会无法正常运行
-                                        User.byId(user100, "d_scope"), LevelEnum.FULL).addItem(
+                                        User.byId(user100, scope_d), LevelEnum.FULL).addItem(
                                         new Permission("abc"))
 
                         )
@@ -85,6 +95,7 @@ public class RoleTest {
         Map<IdType, RoleUserXStruct> singleRoleUserXMap = source.getSingleRoleUserX().get(applicationId, tenantId);
         Map<IdType, RoleUserXStruct> multipleRoleUserXMap = source.getMultipleRoleUserX().get(applicationId, tenantId);
 
+        //测试RoleUserX = roleA User100
         List<RoleUserXStruct> roleAUser100 = getRoleUserXStruct(singleRoleUserXMap, roleAId, user100);
         Assert.assertEquals(roleAUser100.size(), 1);
         Assert.assertTrue(match(new AccessControl(
@@ -93,9 +104,9 @@ public class RoleTest {
                 , roleAUser100.get(0)));
 
 
+        //测试RoleUserX = roleB User100
         List<RoleUserXStruct> roleAUser101 = getRoleUserXStruct(singleRoleUserXMap, roleAId, user101);
         Assert.assertEquals(roleAUser101.size(), 0);
-
         List<RoleUserXStruct> roleBUser100 = getRoleUserXStruct(multipleRoleUserXMap, roleBId, user100);
         Assert.assertEquals(roleBUser100.size(), 1);
         Assert.assertTrue(match(new AccessControl(
@@ -103,6 +114,7 @@ public class RoleTest {
                                 , false, false, false, false, false})
                 , roleBUser100.get(0)));
 
+        //测试RoleUserX = roleA User101
         List<RoleUserXStruct> roleBUser101 = getRoleUserXStruct(multipleRoleUserXMap, roleBId, user101);
         Assert.assertEquals(roleBUser101.size(), 1);
         Assert.assertTrue(match(new AccessControl(
@@ -110,13 +122,43 @@ public class RoleTest {
                                 , false, false, false, false, false})
                 , roleBUser101.get(0)));
 
-        List<RoleUserXStruct> roleAllPermissionIdUser100 = getRoleUserXStruct(singleRoleUserXMap
+        //测试RoleUserX = roleAllPermission User100
+        List<RoleUserXStruct> roleAllPermissionUser100 = getRoleUserXStruct(singleRoleUserXMap
                 , roleAllPermissionId, user100);
-        Assert.assertEquals(roleAllPermissionIdUser100.size(), 1);
+        Assert.assertEquals(roleAllPermissionUser100.size(), 1);
         Assert.assertTrue(match(new AccessControl(
                         new boolean[]{true, true, true, true, true
                                 , true, true, true, true, true})
-                , roleAllPermissionIdUser100.get(0)));
+                , roleAllPermissionUser100.get(0)));
+
+        //--------------------------------------------------------------------------------------------
+        Map<IdType, ScopeRoleUserXStruct> scopeSingleRoleUserXMap = source.getScopeSingleRoleUserX().get(applicationId, tenantId);
+        Map<IdType, ScopeRoleUserXStruct> scopeMultipleRoleUserXMap = source.getScopeMultipleRoleUserX().get(applicationId, tenantId);
+
+        //测试ScopeRoleUserX = roleA User100
+        Set<String> scopesOfRoleAUser100 = getScopesOfRoleUser(scopeSingleRoleUserXMap, roleAId, user100);
+        Assert.assertTrue(match(scopesOfRoleAUser100, new String[]{scope_a, scope_b, scope_c}));
+
+        //测试ScopeRoleUserX = roleB User100
+        Set<String> scopesOfRoleBUser100 = getScopesOfRoleUser(scopeMultipleRoleUserXMap, roleBId, user100);
+        Assert.assertTrue(match(scopesOfRoleBUser100, new String[]{scope_null}));
+
+        //测试ScopeRoleUserX = roleB User101
+        Set<String> scopesOfRoleBUser101 = getScopesOfRoleUser(scopeMultipleRoleUserXMap, roleBId, user101);
+        Assert.assertTrue(match(scopesOfRoleBUser101, new String[]{scope_null}));
+
+        //测试ScopeRoleUserX = roleAllPermission User100
+        Set<String> scopesOfRoleAllPermissionUser100 = getScopesOfRoleUser(scopeSingleRoleUserXMap, roleAllPermissionId, user100);
+        Assert.assertTrue(match(scopesOfRoleAllPermissionUser100, new String[]{scope_d}));
+
+        //--------------------------------------------------------------------------------------------
+        //测试RoleOther
+        Map<IdType, RoleOtherXStruct> scopeSingleRoleOtherXMap = source.getSingleRoleOtherX().get(applicationId, tenantId);
+        List<RoleOtherXStruct> listOfRoleAOther = getRoleOther(scopeSingleRoleOtherXMap, roleAId);
+        Assert.assertEquals(listOfRoleAOther.size(), 1);
+        Assert.assertTrue(match(new Access(
+                        new boolean[]{true, false, false, false, false})
+                , listOfRoleAOther.get(0)));
     }
 
     private List<RoleUserXStruct> getRoleUserXStruct(Map<IdType, RoleUserXStruct> roleUserXMap, String roleId, String userId) {
@@ -126,6 +168,40 @@ public class RoleTest {
                         && o.getRoleId().equals(IdUtils.createId(roleId).getValue()))
                 .collect(Collectors.toList());
         return result;
+    }
+
+    private Set<String> getScopesOfRoleUser(Map<IdType, ScopeRoleUserXStruct> scopeRoleUserXMap
+            , String roleId, String userId) {
+        List<ScopeRoleUserXStruct> list = scopeRoleUserXMap.
+                values().stream().filter(
+                o -> o.getUserId().equals(IdUtils.createUserId(userId).getValue())
+                        && o.getRoleId().equals(IdUtils.createId(roleId).getValue()))
+                .collect(Collectors.toList());
+
+        Set<String> result = list.stream().map(ScopeRoleUserXPrototype::getScope).collect(Collectors.toSet());
+        return result;
+    }
+
+    private List<RoleOtherXStruct> getRoleOther(Map<IdType, RoleOtherXStruct> scopeRoleOtherXMap, String roleId) {
+        List<RoleOtherXStruct> result = scopeRoleOtherXMap.
+                values().stream().filter(
+                o -> o.getRoleId().equals(IdUtils.createId(roleId).getValue()))
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    private boolean match(Set<String> set, String[] array) {
+
+        if (set.size() != array.length) {
+            return false;
+        }
+
+        for (String item : array) {
+            if (!set.contains(item)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     private boolean match(AccessControl accessControl, RoleUserXStruct roleUserXStruct) {
@@ -157,6 +233,26 @@ public class RoleTest {
             return false;
         }
         if (accessControl.isEnableControl() != roleUserXStruct.isEnableControl()) {
+            return false;
+        }
+        return true;
+    }
+
+
+    private boolean match(Access access, RoleOtherXStruct roleOtherXStruct) {
+        if (access.isViewAccess() != roleOtherXStruct.isViewAccess()) {
+            return false;
+        }
+        if (access.isExecuteAccess() != roleOtherXStruct.isExecuteAccess()) {
+            return false;
+        }
+        if (access.isEditAccess() != roleOtherXStruct.isEditAccess()) {
+            return false;
+        }
+        if (access.isDeleteAccess() != roleOtherXStruct.isDeleteAccess()) {
+            return false;
+        }
+        if (access.isEnableAccess() != roleOtherXStruct.isEnableAccess()) {
             return false;
         }
         return true;
