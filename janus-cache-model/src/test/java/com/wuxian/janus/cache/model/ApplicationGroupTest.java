ee03edaa838af482e786c8ad2c171b53eb679b7e
A1502 <jyl550@qq.com>
2020-06-11 19:15:46 +0800

    微调

--- janus-cache-model/src/test/java/com/wuxian/janus/cache/model/ApplicationGroupTest.java
+++ janus-cache-model/src/test/java/com/wuxian/janus/cache/model/ApplicationGroupTest.java
@@ -6,7 +6,7 @@ import com.wuxian.janus.core.cache.provider.DirectAccessControlSource;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class ApplicationGroupTest {
+class ApplicationGroupTest {
     @Test
     @DisplayName("测试ApplicationGroup构建结果能等价于AccessControlCacheProvider的结果")
     void testInitApplicationTenant() {
