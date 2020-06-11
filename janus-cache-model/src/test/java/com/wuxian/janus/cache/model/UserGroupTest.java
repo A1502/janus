9dd6adc88d46b347a95c8462c75fbf53754329da
A1502 <jyl550@qq.com>
2020-06-11 13:39:33 +0800

    测试编写完成

--- janus-cache-model/src/test/java/com/wuxian/janus/cache/model/UserGroupTest.java
+++ janus-cache-model/src/test/java/com/wuxian/janus/cache/model/UserGroupTest.java
@@ -1,14 +1,30 @@
 package com.wuxian.janus.cache.model;
 
+import com.wuxian.janus.cache.model.extract.id.IdUtils;
 import com.wuxian.janus.cache.model.source.*;
 import com.wuxian.janus.core.basis.ErrorCodeException;
 import com.wuxian.janus.core.cache.provider.DirectAccessControlSource;
+import com.wuxian.janus.core.calculate.AccessControlUtils;
 import com.wuxian.janus.core.critical.Access;
+import com.wuxian.janus.core.critical.AccessControl;
 import com.wuxian.janus.core.critical.LevelEnum;
 import com.wuxian.janus.core.critical.NativeUserGroupEnum;
+import com.wuxian.janus.struct.layer1.RoleUserGroupXStruct;
+import com.wuxian.janus.struct.layer1.ScopeUserGroupUserXStruct;
+import com.wuxian.janus.struct.layer1.UserGroupOtherXStruct;
+import com.wuxian.janus.struct.layer1.UserGroupUserXStruct;
+import com.wuxian.janus.struct.primary.ApplicationIdType;
+import com.wuxian.janus.struct.primary.IdType;
+import com.wuxian.janus.struct.primary.TenantIdType;
+import com.wuxian.janus.struct.prototype.layer1.ScopeUserGroupUserXPrototype;
 import org.junit.Assert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -85,11 +101,11 @@ public class UserGroupTest {
                                                 User.byId(user100), LevelEnum.FULL)
                                         , UserGroup.byId(groupAppAdminId, NativeUserGroupEnum.APPLICATION_ADMIN)
                                                 .setAccess(new Access(new boolean[]{true, true})).addItem(
-                                                        User.byId(user100, scope_null, scope_c), LevelEnum.FULL).addItem(
-                                                        User.byId(user100, scope_a, scope_b), LevelEnum.NONE).addItem(
+                                                        User.byId(user100, scope_null, scope_c), LevelEnum.TWO_POINT_FIVE).addItem(
+                                                        User.byId(user100, scope_a, scope_b), LevelEnum.THREE).addItem(
                                                         Role.byId(roleAId, "roleA", true)
-                                                        , Role.byId(roleBId, "roleB", false)
-                                                        , Role.byId(roleCId, "roleC", false)
+                                                        , Role.byId(roleCId, "roleC", false)).addItem(
+                                                        Role.byId(roleBId, "roleB", false), LevelEnum.HYPO_FULL
                                                 )
                                         , UserGroup.byId(groupAId, "groupA", oot0, oo0).addItem(
                                                 Role.byId(roleBId, false)
@@ -100,7 +116,147 @@ public class UserGroupTest {
 
         DirectAccessControlSource source = TestUtils.extractAndPrint("testMergeUserGroupByApplicationGroup", applicationGroup);
 
+        ApplicationIdType applicationId = IdUtils.createApplicationId(appId);
+        TenantIdType tenantId = IdUtils.createTenantId(tId);
 
+        //UserGroupUserX
+
+        Map<IdType, UserGroupUserXStruct> userGroupUserXMap = source.getUserGroupUserX().get(applicationId, tenantId);
+
+        checkUserGroupUserXStruct(userGroupUserXMap, groupAId, user100, null);
+
+        checkUserGroupUserXStruct(userGroupUserXMap, groupAId, user101, new AccessControl(
+                new boolean[]{true, true, false, false, false
+                        , false, false, false, false, false}));
+
+        checkUserGroupUserXStruct(userGroupUserXMap, groupTenantRootId, user100, new AccessControl(
+                new boolean[]{true, true, true, true, true
+                        , true, true, true, true, true}));
+
+        checkUserGroupUserXStruct(userGroupUserXMap, groupAppAdminId, user100, new AccessControl(
+                new boolean[]{true, true, false, false, false
+                        , true, true, false, false, false}));
+
+        //--------------------------------------------------------------------------------------------------------------------
+
+        //RoleUserGroupX
+
+        Map<IdType, RoleUserGroupXStruct> singleRoleUserGroupXMap = source.getSingleRoleUserGroupX().get(applicationId, tenantId);
+        Map<IdType, RoleUserGroupXStruct> multipleRoleUserGroupXMap = source.getMultipleRoleUserGroupX().get(applicationId, tenantId);
+
+        checkRoleUserGroupXStruct(multipleRoleUserGroupXMap, groupAppAdminId, roleAId, new AccessControl(
+                new boolean[]{true, true, false, false, false
+                        , false, false, false, false, false})
+        );
+
+        checkRoleUserGroupXStruct(singleRoleUserGroupXMap, groupAppAdminId, roleBId, new AccessControl(
+                new boolean[]{true, false, true, true, true
+                        , true, true, true, true, true})
+        );
+
+        checkRoleUserGroupXStruct(singleRoleUserGroupXMap, groupAppAdminId, roleCId, new AccessControl(
+                new boolean[]{true, true, false, false, false
+                        , false, false, false, false, false})
+        );
+
+        checkRoleUserGroupXStruct(singleRoleUserGroupXMap, groupAId, roleBId, new AccessControl(
+                new boolean[]{true, true, false, false, false
+                        , false, false, false, false, false})
+        );
+
+        //--------------------------------------------------------------------------------------------------------------------
+
+        //ScopeUserGroupUserX
+        Map<IdType, ScopeUserGroupUserXStruct> scopeUserGroupUserXMap = source.getScopeUserGroupUserX().get(applicationId, tenantId);
+
+        checkScopesOfUserGroupUser(scopeUserGroupUserXMap, groupTenantRootId, user100, scope_null);
+        checkScopesOfUserGroupUser(scopeUserGroupUserXMap, groupAppAdminId, user100, scope_a, scope_b, scope_c, scope_null);
+        checkScopesOfUserGroupUser(scopeUserGroupUserXMap, groupAId, user101, scope_d);
+
+        //--------------------------------------------------------------------------------------------------------------------
+
+        //UserGroupOtherX
+        Map<IdType, UserGroupOtherXStruct> userGroupOtherXMap = source.getUserGroupOtherX().get(applicationId, tenantId);
+
+        checkUserGroupXOther(userGroupOtherXMap, groupTenantRootId, null);
+        checkUserGroupXOther(userGroupOtherXMap, groupAppAdminId, new Access(new boolean[]{true, true}));
+        checkUserGroupXOther(userGroupOtherXMap, groupAId, null);
+    }
+
+    private void checkUserGroupXOther(Map<IdType, UserGroupOtherXStruct> userGroupOtherXMap, String userGroupId, Access access) {
+        List<UserGroupOtherXStruct> list = getUserGroupXOther(userGroupOtherXMap, userGroupId);
+        if (access != null) {
+            Assert.assertEquals(list.size(), 1);
+            Assert.assertTrue(AccessControlUtils.matchWithAccess(list.get(0), access));
+        } else {
+            Assert.assertEquals(list.size(), 0);
+        }
+    }
+
+    private void checkScopesOfUserGroupUser(Map<IdType, ScopeUserGroupUserXStruct> scopeUserGroupUserXMap
+            , String roleId, String userId, String... scopes) {
+        Set<String> set = getScopesOfUserGroupUser(scopeUserGroupUserXMap, roleId, userId);
+        Assert.assertTrue(TestUtils.match(set, scopes));
+    }
+
+    private void checkUserGroupUserXStruct(Map<IdType, UserGroupUserXStruct> userGroupUserXMap
+            , String userGroupId, String userId, AccessControl accessControl) {
+        List<UserGroupUserXStruct> list = getUserGroupUserXStruct(userGroupUserXMap, userGroupId, userId);
+        if (accessControl != null) {
+            Assert.assertEquals(list.size(), 1);
+            Assert.assertTrue(AccessControlUtils.matchWithAccessControl(list.get(0), accessControl));
+        } else {
+            Assert.assertEquals(list.size(), 0);
+        }
+    }
+
+    private void checkRoleUserGroupXStruct(Map<IdType, RoleUserGroupXStruct> roleUserGroupXMap
+            , String userGroupId, String roleId, AccessControl accessControl) {
+        List<RoleUserGroupXStruct> list = getRoleUserGroupXStruct(roleUserGroupXMap, userGroupId, roleId);
+        if (accessControl != null) {
+            Assert.assertEquals(list.size(), 1);
+            Assert.assertTrue(AccessControlUtils.matchWithAccessControl(list.get(0), accessControl));
+        } else {
+            Assert.assertEquals(list.size(), 0);
+        }
+    }
+
+    private List<UserGroupUserXStruct> getUserGroupUserXStruct(Map<IdType, UserGroupUserXStruct> userGroupUserXMap, String userGroupId, String userId) {
+        List<UserGroupUserXStruct> result = userGroupUserXMap.
+                values().stream().filter(
+                o -> o.getUserId().equals(IdUtils.createUserId(userId).getValue())
+                        && o.getUserGroupId().equals(IdUtils.createId(userGroupId).getValue()))
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    private List<RoleUserGroupXStruct> getRoleUserGroupXStruct(Map<IdType, RoleUserGroupXStruct> roleUserGroupXMap, String userGroupId, String roleId) {
+        List<RoleUserGroupXStruct> result = roleUserGroupXMap.
+                values().stream().filter(
+                o -> o.getRoleId().equals(IdUtils.createId(roleId).getValue())
+                        && o.getUserGroupId().equals(IdUtils.createId(userGroupId).getValue()))
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    private Set<String> getScopesOfUserGroupUser(Map<IdType, ScopeUserGroupUserXStruct> scopeUserGroupUserXMap
+            , String roleId, String userId) {
+        List<ScopeUserGroupUserXStruct> list = scopeUserGroupUserXMap.
+                values().stream().filter(
+                o -> o.getUserId().equals(IdUtils.createUserId(userId).getValue())
+                        && o.getUserGroupId().equals(IdUtils.createId(roleId).getValue()))
+                .collect(Collectors.toList());
+
+        Set<String> result = list.stream().map(ScopeUserGroupUserXPrototype::getScope).collect(Collectors.toSet());
+        return result;
+    }
+
+    private List<UserGroupOtherXStruct> getUserGroupXOther(Map<IdType, UserGroupOtherXStruct> userGroupOtherXMap, String userGroupId) {
+        List<UserGroupOtherXStruct> result = userGroupOtherXMap.
+                values().stream().filter(
+                o -> o.getUserGroupId().equals(IdUtils.createId(userGroupId).getValue()))
+                .collect(Collectors.toList());
+        return result;
     }
 }
 
